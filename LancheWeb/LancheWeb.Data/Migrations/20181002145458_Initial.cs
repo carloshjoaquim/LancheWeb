d00c68bb67d6090a058a4fabb92507af6aab90f0
Carlos Joaquim <carlos.joaquim@dextra-sw.com>
2018-10-02 11:55:26 -0300

    Add-Migration

---
+++ LancheWeb/LancheWeb.Data/Migrations/20181002145458_Initial.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LancheWeb.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredientes",
+                columns: table => new
+                {
+                    IngredienteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Valor = table.Column<decimal>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredientes", x => x.IngredienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lanches",
+                columns: table => new
+                {
+                    LancheId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lanches", x => x.LancheId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredienteLanche",
+                columns: table => new
+                {
+                    IdLanche = table.Column<int>(nullable: false),
+                    IdIngrediente = table.Column<int>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredienteLanche", x => new { x.IdLanche, x.IdIngrediente });
+                    table.ForeignKey(
+                        name: "FK_IngredienteLanche_Ingredientes_IdIngrediente",
+                        column: x => x.IdIngrediente,
+                        principalTable: "Ingredientes",
+                        principalColumn: "IngredienteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredienteLanche_Lanches_IdLanche",
+                        column: x => x.IdLanche,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredienteLanche_IdIngrediente",
+                table: "IngredienteLanche",
+                column: "IdIngrediente");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredienteLanche");
+
+            migrationBuilder.DropTable(
+                name: "Ingredientes");
+
+            migrationBuilder.DropTable(
+                name: "Lanches");
+        }
+    }
+}
